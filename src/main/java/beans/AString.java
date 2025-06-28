7eb033807b837f5fce2e3112f608835d7b419fa2
robertminkler <robertminkler@gmail.com>
2025-06-28 09:34:35 -0700

    remove old test bean

--- src/main/java/beans/AString.java
+++
@@ -1,9 +0,0 @@
-package beans;
-
-import java.io.Serializable;
-public class AString implements Serializable {
-private static final long serialVersionUID = 1L;
-private String str;
-  public String getStr() { return str; }
-  public void setStr(String s) { str = s; }
-  }
